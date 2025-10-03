731de9d88ae2ed203161699cc5779c7fee063e67
anakafeel <hashmisaim037@gmail.com>
2025-10-03 18:59:17 -0400

    add initial event execution logic

--- Assignment 1/interrupts.cpp
+++ Assignment 1/interrupts.cpp
@@ -5,35 +5,80 @@
  *
  */
 
-#include<interrupts.hpp>
+#include <interrupts.hpp>
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv)
+{
 
-    //vectors is a C++ std::vector of strings that contain the address of the ISR
-    //delays  is a C++ std::vector of ints that contain the delays of each device
-    //the index of these elemens is the device number, starting from 0
+    // vectors is a C++ std::vector of strings that contain the address of the ISR
+    // delays  is a C++ std::vector of ints that contain the delays of each device
+    // the index of these elemens is the device number, starting from 0
     auto [vectors, delays] = parse_args(argc, argv);
     std::ifstream input_file(argv[1]);
 
-    std::string trace;      //!< string to store single line of trace file
-    std::string execution;  //!< string to accumulate the execution output
+    std::string trace;     //!< string to store single line of trace file
+    std::string execution; //!< string to accumulate the execution output
 
     /******************ADD YOUR VARIABLES HERE*************************/
 
+    int SWITCH_MODE = 1;      // User mode = 1, Kernel mode = 0
+    int SAVE_CONTEXT = 10;    // Context save time
+    int RESTORE_CONTEXT = 10; // Context restore time
+    int FIND_VECTOR = 1;      // Time to find vector
+    int GET_ISR = 10;         // Time to get ISR
+    const IRET_TIME = 1;      // Time for IRET instruction
+    const int VECTOR_ENTRY_BYTES = 2;
+
+    long long current_time = 0; // Current time in the simulation
 
 
+    // PLEASE DONT MIND THE WEIRD SPACES , THE FORMATTER I USE ADDS THEM AUTOMATICALLY
+    // created an even struct to store event information
+    struct event
+    {
+        enum Type   
+        {
+            CPU,
+            END_IO,
+            SYSCALL
+        };
+        Type type;
+        int dur;
+    };
+
+    // using lambda funciton to crea te a function inside main to convert trace type to event type
+    auto trace_convert = [](std::string type, int dur)
+    {
+        event::Type t;
+
+        if (type == "CPU")
+            t = event::CPU;
+        else if (type == "END_IO")
+            t = event::END_IO;
+        else if (type == "SYSCALL")
+            t = event::SYSCALL;
+        else
+            throw std::invalid_argument("Unknown type");
+
+        return event{t, dur};
+    };
+
+    // using lambda function to create a function inside main to log event execution
+    auto log_event = [&](long long start, long long dur, const std::string &desc)
+    {
+        execution += std::to_string(start) + " " + std::to_string(dur) + " " + desc + "\n";
+    };
+    
     /******************************************************************/
 
-    //parse each line of the input trace file
-    while(std::getline(input_file, trace)) {
+    // parse each line of the input trace file
+    while (std::getline(input_file, trace))
+    {
         auto [activity, duration_intr] = parse_trace(trace);
 
         /******************ADD YOUR SIMULATION CODE HERE*************************/
 
-
-
         /************************************************************************/
-
     }
 
     input_file.close();
